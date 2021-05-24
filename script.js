5ce394315fb8fc348786cf90fda34b75e63a83d2
David Tran <tr.davidt@gmail.com>
2021-05-23 19:28:41 -0700

    Add wave and update nav animation

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ toggleNav = function() {
     if(navLinks[i].style.display !== "block") {
       navLinks[i].style.display = "block";
       navLinks[i].classList.add("animate__animated", "animate__fadeIn");
-      console.log("Nav links displayed")
     }
     else {
       navLinks[i].style.display = "none";
@@ -23,13 +22,15 @@ toggleBtn.addEventListener("click", toggleNav);
 onScrollNav = function() {
   navHeight = document.querySelector(".nav-container").clientHeight;
   navContainer = document.querySelector(".nav-container");
+  navLogo = document.querySelector("#nav-logo img");
 
   if (window.pageYOffset >= navHeight) {
     navContainer.classList.add("scrolled");
-    console.log("Nav goes opaque");
+    navLogo.style.maxWidth = "3vw";
   }
   else {
-    navContainer.classList.remove("scrolled")
+    navContainer.classList.remove("scrolled");
+    navLogo.style.maxWidth = "5vw";
   }
 }
 window.addEventListener("scroll", onScrollNav);
